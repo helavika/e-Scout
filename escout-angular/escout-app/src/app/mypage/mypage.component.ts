3a85b26be474e3a85ef58f10a034c634a7f44c12
danyil <d.voloschuk@ip.net.ua>
2020-04-21 18:06:56 +0300

    added new content for my page

--- escout-angular/escout-app/src/app/mypage/mypage.component.ts
+++ escout-angular/escout-app/src/app/mypage/mypage.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import {AppComponent} from '../app.component';
 import {Scout} from '../interfaces/scout';
+import {Achievement} from '../interfaces/achievement';
 
 @Component({
   selector: 'app-mypage',
   templateUrl: './mypage.component.html',
-  styleUrls: ['./mypage.component.css']
+  styleUrls: [
+    './mypage.component.css',
+    '../app.component.css',
+  ]
 })
+
 export class MypageComponent implements OnInit {
 
   scout: Scout;
+  achievements: Achievement[];
   constructor(private appComponent: AppComponent) { }
 
   ngOnInit(): void {
